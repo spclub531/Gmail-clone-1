91db6b8b36d0cd331b1d8a23bcfbe77b57897ec4
suraj <surajlalapatel@gmail.com>
2024-04-02 10:43:25 +0530

    first commit

--- script.js
+++ script.js
@@ -1,15 +1,146 @@
-// Get the textarea element
-const textarea = document.getElementById("myTextarea");
+"use strict";
 
-// Function to save the textarea content to local storage
-function saveToLocalStorage() {
-    localStorage.setItem("savedText", textarea.value);
-}
+const data = [
+  // 1
+  {
+    sender: "Sadam",
+    date: "Dec, 12",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 2
+  {
+    sender: "Twitter",
+    date: "Dec, 22",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  //  3
+  {
+    sender: "Yahye",
+    date: "Dec, 14",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 4
+  {
+    sender: "Sadam",
+    date: "Jan, 11",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 5
+  {
+    sender: "Qadar",
+    date: "Dec, 12",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 6
+  {
+    sender: "John",
+    date: "Oct, 26",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 7
+  {
+    sender: "Slaf",
+    date: "Dec, 21",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 8
+  {
+    sender: "Sadam",
+    date: "Dec, 10",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 9
+  {
+    sender: "Farah",
+    date: "May, 20",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 10
+  {
+    sender: "Alex",
+    date: "Feb, 12",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 11
+  {
+    sender: "Alex",
+    date: "Jun, 25",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 12
+  {
+    sender: "Moe",
+    date: "Sep, 28",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 13
+  {
+    sender: "Jey",
+    date: "Jan, 17",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 14
+  {
+    sender: "Adam",
+    date: "Nov, 15",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 15
+  {
+    sender: "Joe",
+    date: "Apr, 23",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 16
+  {
+    sender: "Sadam",
+    date: "Jul, 28",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+  // 17
+  {
+    sender: "Slef",
+    date: "Dec, 31",
+    message:
+      " Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem libero delectus sint delectus sint adipisicing ...",
+  },
+];
 
-// Check if there's existing data in local storage and populate the textarea
-if (localStorage.getItem("savedText")) {
-    textarea.value = localStorage.getItem("savedText");
-}
+const messages = document.querySelector(".messages_area");
 
-// Add an event listener to the textarea for input changes and save to local storage
-textarea.addEventListener("input", saveToLocalStorage);
+const render = function (data) {
+  messages.innerHTML = data
+    .map(
+      (d) => `<div class="messages">
+  <div class="messages_left">
+    <div class="check check_2">
+      <img src="./img/checkbox.png" alt="" />
+      <span> <ion-icon name="checkmark"></ion-icon> </span>
+    </div>
+    <ion-icon name="star-outline" class="hover star"></ion-icon>
+    <span>${d.sender}</span>
+  </div>
+  <div class="messages_content">${d.message}</div>
+  <div class="messages_date">${d.date}</div>
+</div>`
+    )
+    .join(" ");
+};
+
+render(data);
